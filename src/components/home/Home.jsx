8a6f4ff65ad99be6be24b50edfd44bab36497630
Paolinsky <paolinskypdtn@outlook.com>
2020-02-07 17:52:50 -0500

    Logo and icon added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import '../../styles/home.scss'
+import PaolinskyLogo from '../../assets/PaolinskyLogo.png'
 
 class Home extends React.Component {
     render() {
         return (
             <div className="home">
-                <h1 id="paolo">Paolo</h1>
-                <h1 id="torregrosa">Torregrosa</h1>
-                <h3>Software developer</h3>
+                <img className="logo" src={PaolinskyLogo} alt="Paolinsky Logo"/>
+                <div className="name">
+                    <h1>Paolo</h1>
+                    <h1>Torregrosa</h1>
+                    <h3>Software developer</h3>
+                </div>
             </div>
         );
     }
