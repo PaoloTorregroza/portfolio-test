2029fb67fd9c9821290141b9d4e9d511dd7509ca
Paolinsky <paolinskypdtn@outlook.com>
2020-02-07 16:02:58 -0500

    Designer recomendations aplied

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,7 +5,8 @@ class Home extends React.Component {
     render() {
         return (
             <div className="home">
-                <h1>Paolo<br/>Torregrosa</h1>
+                <h1 id="paolo">Paolo</h1>
+                <h1 id="torregrosa">Torregrosa</h1>
                 <h3>Software developer</h3>
             </div>
         );
