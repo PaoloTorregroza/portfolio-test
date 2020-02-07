c66a115c69357b8d0582ccd1e4c73c1105b7710f
Paolinsky <paolinskypdtn@outlook.com>
2020-02-07 18:54:46 -0500

    Contact component, and social media icons created and placed on home

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../styles/contact.scss'
+import Twitter from '../../assets/twitter.png';
+import Github from '../../assets/github.png';
+import Linkedin from '../../assets/linkedin.png';
+
+class Contact extends React.Component {
+    render() {
+        return (
+            <div className="imgContainer">
+                <a target="_blank" href="https://twitter.com/TorregrozaPaolo"><img id="twitter" src={Twitter} alt="@TorregrozaPaolo"/></a>
+                <a target="_blank" href="https://github.com/PaoloTorregroza"><img id="github" src={Github} alt="PaoloTorregroza"/></a>
+                <a target="_blank" href="https://www.linkedin.com/in/paolo-donato-torregrosa-navarro-61947a191/"><img id="linkedin" src={Linkedin} alt="Paolo Torregrosa"/></a>
+            </div>
+        );
+    }
+}
+
+export default Contact;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../styles/home.scss'
 import PaolinskyLogo from '../../assets/PaolinskyLogo.png'
+import Contact from '../contact/Contact';
 
 class Home extends React.Component {
     render() {
@@ -12,6 +13,7 @@ class Home extends React.Component {
                     <h1>Torregrosa</h1>
                     <h3>Software developer</h3>
                 </div>
+                <Contact />
             </div>
         );
     }
