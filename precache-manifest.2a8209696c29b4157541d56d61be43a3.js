2bfe9484e323c063d2a02ae7a5c670bf46644c49
Paolinsky <Paolodydtorregrosa@gmail.com>
2020-04-21 15:34:12 -0500

    Updates

---
+++ precache-manifest.2a8209696c29b4157541d56d61be43a3.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e8a9ab8039d0d11d32da405f2e18d90",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c6677b88c87e97100363",
+    "url": "/portfolio/static/css/main.5be15aba.chunk.css"
+  },
+  {
+    "revision": "ff09db167214f64687b6",
+    "url": "/portfolio/static/js/2.c5ce907b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.c5ce907b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6677b88c87e97100363",
+    "url": "/portfolio/static/js/main.514ca3dc.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "b00f737f3f49ef5a0daba40aff3ce531",
+    "url": "/portfolio/static/media/PaolinskyLogo.b00f737f.png"
+  },
+  {
+    "revision": "4bb12b0fb48830113ea21198471cfc3c",
+    "url": "/portfolio/static/media/Paolo.4bb12b0f.jpg"
+  },
+  {
+    "revision": "b4555209f8aa9c4b2d5277e44b73a0ff",
+    "url": "/portfolio/static/media/c.b4555209.png"
+  },
+  {
+    "revision": "0a25407f799d41e976243cfb66f5e46c",
+    "url": "/portfolio/static/media/csharp.0a25407f.png"
+  },
+  {
+    "revision": "7e8be056b18757a0d4ab914d69e0dac8",
+    "url": "/portfolio/static/media/game-of-life.7e8be056.png"
+  },
+  {
+    "revision": "628faa9826b228378a4b549e86d1f3f0",
+    "url": "/portfolio/static/media/github.628faa98.png"
+  },
+  {
+    "revision": "a470baa362b39be8f8d3f942cb2b1f14",
+    "url": "/portfolio/static/media/godot.a470baa3.svg"
+  },
+  {
+    "revision": "975f6d4b91c20168391e0490fcaf213a",
+    "url": "/portfolio/static/media/golang.975f6d4b.png"
+  },
+  {
+    "revision": "f6b1b46111ec9406721957ff80f2ec78",
+    "url": "/portfolio/static/media/htmlcssjs.f6b1b461.png"
+  },
+  {
+    "revision": "6f3e502fd64e6023a64359d47cc1c04d",
+    "url": "/portfolio/static/media/java.6f3e502f.png"
+  },
+  {
+    "revision": "87076c7b131914a7424f836d27247602",
+    "url": "/portfolio/static/media/math.87076c7b.png"
+  },
+  {
+    "revision": "b337a2fb0b1289e6757e632dc766ff6b",
+    "url": "/portfolio/static/media/node.b337a2fb.svg"
+  },
+  {
+    "revision": "21c0092065c00c79f7287ea7bce6f090",
+    "url": "/portfolio/static/media/portfolioImg.21c00920.png"
+  },
+  {
+    "revision": "55747c4b53ae8fd363aaec3841d5f48a",
+    "url": "/portfolio/static/media/postgresql.55747c4b.webp"
+  },
+  {
+    "revision": "826ecc0aeca6b13d5120408efbb8198c",
+    "url": "/portfolio/static/media/python.826ecc0a.png"
+  },
+  {
+    "revision": "638be90904f9f9248d3c69cb2992c562",
+    "url": "/portfolio/static/media/react.638be909.png"
+  },
+  {
+    "revision": "f54d6da1eac5c41f1a88546b78bc4451",
+    "url": "/portfolio/static/media/sass.f54d6da1.png"
+  },
+  {
+    "revision": "2b47c276f241833a7f1350169671f059",
+    "url": "/portfolio/static/media/vue.2b47c276.png"
+  }
+]);
